b46f50da2d9e7b743a71abdefd9a74621f6f8b00
danielgarofalo <garofalodaniel@gmail.com>
2022-10-16 11:34:29 -0500

    Menus completed

--- src/Simplilearn_CoreJava_FileManagement/Main.java
+++ src/Simplilearn_CoreJava_FileManagement/Main.java
@@ -1,7 +1,6 @@
 package Simplilearn_CoreJava_FileManagement;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,74 +10,183 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
+        //Main method controllers
         boolean quitProgram = false;
-        int choice1 = 0;
+        int choice1;
+        String userInput;
+
+        //Initial menu will display
+        initiatingMenu();
+
 
         while (!quitProgram){
-            initiatingMenu();
-            choice1 = scanner.nextInt();
+            userInput = scanner.nextLine();
 
+            //Input verification
+            while (containsString(userInput)){
+                wrongOptionMessage();
+                initiatingMenu();
+                userInput = scanner.nextLine();
+            }
+
+            choice1 = Integer.parseInt(userInput);
+
+            //Menu logic
             switch (choice1) {
                 case 1 -> folderSelection();
                 case 2 -> quitProgram = true;
             }
         }
-
-
     }
 
-    private static File folderSelection() throws Exception {
 
-        File fileLocation;
 
-        System.out.println("Please, type the location for your files");
-        fileLocation = new File(scanner.next());
-
-        if(fileLocation.exists()){
-            showMainMenu();
-        } else {
-            System.out.println("Location does not exist. Make sure the location exists in your computer.");
-        }
-
-        return fileLocation;
-
-    }
-
+    //Menus
     private static void initiatingMenu(){
         System.out.println("------LockedMe.com------");
         System.out.println("Enter your choice:");
-        System.out.println("1.Input folder location:");
-        System.out.println("2.Exit Program:");
+        System.out.println("1.Input folder location");
+        System.out.println("2.Exit Program");
+    }
+
+    private static void mainMenu(){
+        System.out.println("------Main Menu------");
+        System.out.println("Select your option:");
+        System.out.println(" 1.List files in your location");
+        System.out.println(" 2.File Operations");
+        System.out.println(" 3.Exit Program");
+    }
+
+    private static void opsMenu(){
+        System.out.println("------Operations Menu------");
+        System.out.println("Select your options:");
+        System.out.println("1.Add a new file");
+        System.out.println("2.Delete a file");
+        System.out.println("3.Search for a file");
+        System.out.println("4.Exit to Main Menu");
+    }
+
+
+    private static void folderSelection() {
+
+        File fileLocation = null;
+
+        System.out.println("Please, type the location for your files");
+        fileLocation = new File(scanner.nextLine());
+
+        if(fileLocation.exists()){
+            System.out.println("Location found. Taking you to the main menu.");
+            showMainMenu();
+        } else {
+            System.out.println("Location does not exist. Make sure the location exists in your computer.");
+//            initiatingMenu();
+        }
+
     }
 
 
     private static void showMainMenu() {
 
-        boolean quitMain = false;
+        boolean quitToInitiating = false;
+        String userInput2;
         int choice2 = 0;
 
-        System.out.println("\n ------Main Menu------");
-        System.out.println("\n Select your option:");
-        System.out.println("\n 1.List files in your location");
-        System.out.println("\n 2.File Operations");
-        System.out.println("\n 3.Exit Program");
+        while (!quitToInitiating){
+            mainMenu();
 
-        switch (choice2){
-            case 1 -> listFiles();
-            case 2 -> showOperationsMenu();
-            case 3 -> quitMain = true;
+            userInput2 = scanner.nextLine();
+            while (containsString(userInput2)){
+                wrongOptionMessage();
+                mainMenu();
+                userInput2 = scanner.nextLine();
+            }
+
+            choice2 = Integer.parseInt(userInput2);
+
+            switch (choice2) {
+                case 1 -> listFiles();
+                case 2 -> showOperationsMenu();
+                case 3 -> quitToInitiating = true;
+                default -> wrongOptionMessage();
+            }
         }
-
     }
+
+
+
 
     private static void showOperationsMenu(){
 
-        System.out.println("\n Select your options:");
-        System.out.println("\n 1.Add a new file");
-        System.out.println("\n 2.Delete a file");
-        System.out.println("\n 3.Search for a file");
-        System.out.println("\n 3.Exit to Main Menu");
+        boolean quitToMain = false;
+        String userInput3;
+        int choice3 = 0;
+
+        while (!quitToMain){
+            opsMenu();
+
+            userInput3 = scanner.nextLine();
+            while (containsString(userInput3)){
+                wrongOptionMessage();
+                opsMenu();
+                userInput3 = scanner.nextLine();
+            }
+
+
+            choice3 = Integer.parseInt(userInput3);
+
+            switch (choice3){
+                case 1 -> addFile();
+                case 2 -> deleteFile();
+                case 3 -> searchFile();
+                case 4 -> quitToMain = true;
+                default -> wrongOptionMessage();
+            }
+        }
+    }
+
+    private static void searchFile() {
+    }
+
+    private static void deleteFile() {
+        
+    }
+
+    private static void addFile() {
+    }
+
+    private static void listFiles() {
     }
 
 
+
+    //Utility functions
+    //Verification of input
+    public static boolean containsString(String userInput){
+
+        boolean hasString = false;
+        int index = 0;
+
+        while (index < userInput.length()){
+            if (!(userInput.charAt(index) > '0' && userInput.charAt(index) < '9')) {
+                hasString = true;
+                break;
+            }
+            index++;
+        }
+
+        return hasString;
+    }
+
+    //Wrong option message
+    private static void wrongOptionMessage() {
+        System.out.println("Wrong option! Please, select one of the items in the prompt");
+    }
+
 }
+
+
+
+
+
+
+
